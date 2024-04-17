1b299563368c3f689ac57c84d06bc27402589d73
Vanessa Albrecht <vanialbrecht@hotmail.com>
2024-04-17 15:58:20 +0200

    added redux for NewEvents and added components addEventInformation, without funcionality

--- frontend/src/store/index.jsx
+++ frontend/src/store/index.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice'
+import newEventSlice from './slices/newEventSlice'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    event: newEventSlice,
   },
 })
 
