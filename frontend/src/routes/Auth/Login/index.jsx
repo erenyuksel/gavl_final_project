fb10d83c12a94a2b799af4288f9175363fff9f17
saschajb <sascha@mbp-von-sascha.home>
2024-04-17 10:42:02 +0200

    adding axios

--- frontend/src/routes/Auth/Login/index.jsx
+++ frontend/src/routes/Auth/Login/index.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react"
 
 const Login = () => {
-  const [formData, setFormData] = useState()
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
   const [error, setError] = useState()
 
 
   const handleLogin = async (e) => {
     e.preventDefault()
     setError('')
+
     try {
-      console.log('test')
-      } catch (error) {
+      const response = await JudgeAxios
+    } catch (error) {
       setError(error.response.data.detail)
+    } finally {
+        setFormData({
+          email: '',
+          password: ''
+        })
     }
   }
 
@@ -32,15 +41,24 @@ const Login = () => {
         <label className="input input-bordered flex items-center gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
           <input 
-          type="text" 
+          type="text"
+          name="email"
           className="grow" 
           placeholder="Email"
           value={formData.email}
+          onChange={handleInputChange}
            />
         </label>
         <label className="input input-bordered flex items-center gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-          <input type="password" className="grow" value="password" />
+          <input 
+          type="password" 
+          name="password"
+          className="grow" 
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleInputChange}
+           />
         </label>
         <button className="btn btn-primary" onClick={handleLogin}>Login</button>
       </div>
