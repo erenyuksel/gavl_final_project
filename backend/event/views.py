6f096bc3aeb3c0df827e6d8a8078989b3211e7fa
Andy Acosta <andyacostaa@gmail.com>
2024-04-17 15:47:44 +0200

    updated events app

--- backend/event/views.py
+++ backend/event/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import DestroyAPIView, CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.generics import DestroyAPIView, CreateAPIView, ListAPIView
 
 from .models import Event
 from .serializers import EventSerializer
