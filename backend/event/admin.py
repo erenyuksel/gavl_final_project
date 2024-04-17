cb1dac1b8d51a6bf013261a879e237b64fff17e1
Andy Acosta <andyacostaa@gmail.com>
2024-04-17 11:23:04 +0200

    updated events

--- backend/event/admin.py
+++ backend/event/admin.py
@@ -6,7 +6,7 @@ from .models import Event
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = (
-    'name', 'start_date', 'end_date', 'description', 'created_at', 'updated_at')
+        'name', 'start_date', 'end_date', 'description', 'created_at', 'updated_at')
     search_fields = ('name', 'description',)
 
     # 'projects', 'owner', 'sponsors', 'rubrics', 'judges',
--- backend/event/models.py
+++ backend/event/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+
 # from backend.user.models import User, Organisation
 
 User = get_user_model()
