1ffc077765de509054fd0a0b411b3b6e2d04426d
daria.zorina <daria.zorina@yahoo.com>
2024-04-17 09:53:33 +0200

    users/all, user/{id}, permissions are in process

---
+++ backend/user/urls.py
@@ -0,0 +1,9 @@
+from user.views import (ListUsersView, ReadUpdateDeleteMyUserView, RetrieveUserView)
+
+from django.urls import path
+
+user_urlpatterns = [
+    path("users/", ListUsersView.as_view()),
+    # path("users/me/", ReadUpdateDeleteMyUserView.as_view()),
+    path("users/<int:user_id>/", RetrieveUserView.as_view()),
+]
