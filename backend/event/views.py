7a8a9ec0594d0968f3ed0c494519561605e06de7
Andy Acosta <andyacostaa@gmail.com>
2024-04-17 15:41:58 +0200

    updated events app

--- backend/event/views.py
+++ backend/event/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import DestroyAPIView, CreateAPIView, ListAPIView
+from rest_framework.generics import DestroyAPIView, CreateAPIView, ListAPIView, RetrieveAPIView
 
 from .models import Event
 from .serializers import EventSerializer
@@ -25,11 +25,17 @@ class DeleteEventView(DestroyAPIView):
 
 
 class UserEventListView(ListAPIView):
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = []
+    # permission_classes = []
 
-    def get_queryset(self):
-        user = User.objects.get(id=self.kwargs['user_id'])
-        return Event.objects.filter(user=user)
+# class EventDetailView(RetrieveAPIView):
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer
+#
+#     def get_object(self):
+#         event_id = self.kwargs['pk']
+#         user = self.request.user
+#         return self.get_queryset().filter(id=event_id, user=user).first()
 
 # Get specific Event View --> ListAPIView
