b01099aa8ac79be12d57d1d9a6c294aea9f585ff
Andy Acosta <andyacostaa@gmail.com>
2024-04-18 15:11:26 +0200

    updated events

--- backend/event/serializers.py
+++ backend/event/serializers.py
@@ -5,5 +5,5 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['id', 'name', 'description', 'start_date', 'end_date', 'created_at', 'updated_at', ]
+        fields = ['id', 'name', 'rubrics', 'start_date', 'end_date', 'projects', 'owner', 'sponsors', 'description', 'project_file_structure', 'judges', 'created_at', 'updated_at', ]
 
