7e9b3803f7718795d187f302669354826928e9fb
Andy Acosta <andyacostaa@gmail.com>
2024-04-18 17:43:44 +0200

    updated evaluations

--- backend/event/models.py
+++ backend/event/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    rubrics = models.ManyToManyField(to='evaluation.Rubric', related_name='rubric_event', blank=True)
+    rubrics = models.ForeignKey(to='evaluation.Rubric', on_delete=models.CASCADE, related_name='rubric_event', blank=True, null=True)
     start_date = models.DateField()
     end_date = models.DateField()
     projects = models.ManyToManyField(to='contestant_project.ContestantProject', related_name='event_projects',
                                       blank=True,)
-    owner = models.ForeignKey(to='user.Organisation', on_delete=models.CASCADE, related_name='organisation_admin',
+    owner = models.ForeignKey(to='user.Organisation', on_delete=models.DO_NOTHING, related_name='organisation_admin',
                               blank=True, null=True)
     sponsors = models.ManyToManyField(to='user.Organisation', related_name='sponsored_event', blank=True)
     description = models.TextField()
