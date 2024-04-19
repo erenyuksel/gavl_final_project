c403de1c5c24abdf69feeb8df525c9e7aa4e7e1c
daria.zorina <daria.zorina@yahoo.com>
2024-04-19 12:56:29 +0200

    endpoints: get user by his token(invitation link), deactivate used token

--- backend/registration/utils.py
+++ backend/registration/utils.py
@@ -1,5 +1,9 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from project import settings
 from registration.models import Token
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.backends import TokenBackend
 
 
 def get_and_store_tokens_for_user(user):
@@ -16,3 +20,19 @@ def get_and_store_tokens_for_user(user):
         'access': str(access),
         'token_id': token.id  # Optionally, keep track of the token ID
     }
+
+
+def is_token_valid(token):
+    try:
+        # Assuming default settings for the token backend, you may need to adjust based on your settings
+        token_backend = TokenBackend(algorithm="HS256", signing_key=settings.SECRET_KEY)
+        valid_data = token_backend.decode(token, verify=True)
+        return True
+
+    except InvalidToken as e:
+        print("Invalid token:", str(e))
+        return False
+
+    except TokenError as e:
+        print("Token error:", str(e))
+        return False
