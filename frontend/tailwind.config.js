67cbb7a6e6715f40338fe36d77ae88bc6a1407c0
Vanessa Albrecht <vanialbrecht@hotmail.com>
2024-04-24 14:57:08 +0200

    Added Header for constestants

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: '#4AC6D2',
-      secondary: '#9E15BF',
-      logo: '#57D5C0'
+    extend: {
+      colors: {
+        primary: '#4AC6D2',
+        secondary: '#9E15BF',
+        logo: '#57D5C0',
+      },
     },
-    extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#4AC6D2',
+          secondary: '#9E15BF',
+        },
+      },
+    ],
+  },
 }
