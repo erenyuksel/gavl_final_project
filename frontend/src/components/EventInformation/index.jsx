bc2a86bec3ecc89f33caa58936787a173d6d0566
saschajb <sascha@mbp-von-sascha.home>
2024-04-26 13:11:50 +0200

    added event header and changed project card styling

--- frontend/src/components/EventInformation/index.jsx
+++ frontend/src/components/EventInformation/index.jsx
@@ -1,71 +1,29 @@
 const EventInformationSection = ({ event }) => {
+
+  const getStatus = (startDate, endDate) => {
+    const currentDate = new Date();
+  
+    if (currentDate < startDate) {
+      return <p className="badge badge-lg bg-orange-300">upcoming</p>
+    } else if (currentDate >= startDate && currentDate <= endDate) {
+      return <p className="badge badge-lg bg-green-400 ml-3">active</p>
+    } else {
+      return <p className="badge badge-lg bg-red-300 ml-3">expired</p>
+    }
+  }
+
   return (
     <>
-      <h1>Event Information</h1>
-      <label className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Event name:</span>
-        </div>
-        <input
-          type="text"
-          name="name"
-          value={event.name}
-          placeholder="Event name"
-          className="input input-ghost w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Event description:</span>
-        </div>
-        <input
-          type="text"
-          name="description"
-          value={event.description}
-          placeholder="Event description"
-          className="input input-ghost w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Created date:</span>
-        </div>
-        <input
-          type="text"
-          name="created_at"
-          value={event.created_at}
-          placeholder="Created date"
-          className="input input-ghost w-full max-w-xs"
-        />
-      </label>
-      <div className="flex">
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">Event start date:</span>
-          </div>
-          <input
-            type="text"
-            name="start_date"
-            value={event.start_date}
-            placeholder="Event start date"
-            className="input input-ghost w-full max-w-xs"
-          />
-        </label>
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">Event end date:</span>
-          </div>
-          <input
-            disabled
-            type="text"
-            name="end_date"
-            value={event.end_date}
-            placeholder="Event end date"
-            className="input input-ghost w-full max-w-xs disabled:bg-inherit disabled:border-none"
-          />
-        </label>
+    <div className="flex flex-col w-100 items-center">
+      <h1>{event.name}</h1>
+      <div>
+        <h2>{event.description}</h2>
       </div>
-      <div className="border-t border-gray-100"></div>
+      <div className="flex flex-row align-middle">
+        <h4>Event status:</h4>
+        {getStatus(event.start_date, event.end_date)}
+      </div>
+    </div>
     </>
   )
 }
