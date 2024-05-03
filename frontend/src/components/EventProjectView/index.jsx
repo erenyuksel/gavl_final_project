0c94922247ca082be92fffe61854624db7a44304
saschajb <sascha@mbp-von-sascha.home>
2024-05-03 10:44:44 +0200

    added sorting to project cards and fixing event status

--- frontend/src/components/EventProjectView/index.jsx
+++ frontend/src/components/EventProjectView/index.jsx
@@ -2,11 +2,21 @@ import EventProjectCard from "../EventProjectCard"
 
 const EventProjectView = ( {event}) => {
 
+  const sortedProjects = event.projects ? event.projects.slice().sort((a, b) => {
+    if (a.name < b.name) {
+      return -1
+    }
+    if (a.name > b.name) {
+      return 1
+    }
+    return 0;
+  }) : []
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 m-5">
-    {event.projects && (
+    {(event.projects && sortedProjects) && (
       <>
-      {event.projects.map(project => {
+      {sortedProjects.map(project => {
         return <EventProjectCard project={project} key={project.id}/>
       })}
       </>
